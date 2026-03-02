fdb2a68223cb8e83773154dda37b5667c35dd2da
gabrielnavia <gabrielnavia@unicauca.edu.co>
2026-03-02 17:25:17 -0500

    separé variables de control y variables de retardo, corregí el uso incorrecto de etiquetas como memoria, ahora está de forma explicita el OSCCON a 8MHZ, corregi el problema que impedia ejecutar el ciclo de 2 segundos

--- ejercicio_2/Ejercicio2.asm
+++ ejercicio_2/Ejercicio2.asm
@@ -1,7 +1,7 @@
 ;=========================================================
 ; Código en Assembler para PIC18F4550
 ; Secuencia de parpadeo en RB0:
-; 5 parpadeos de 1 segundo
+; 5 parpadeos de 1 segundo (10 segundos totales)
 ; Luego 2 parpadeos de 2 segundos
 ; Reinicia la secuencia
 ; Usa retardos sin interrupciones ni Timer0
@@ -9,16 +9,16 @@
 ; Ensamblador: MPLAB XC8 3.0
 ;=========================================================
 
-    #include <xc.inc>   ; Definiciones para PIC18F4550
+    #include <xc.inc>   ; Definiciones del ensamblador para PIC18F4550
 
 ;=========================================================
 ; Bits de Configuración (Fuses)
 ;=========================================================
 
-    CONFIG  FOSC = INTOSCIO_EC   ; Oscilador interno
-    CONFIG  WDT = OFF            ; Watchdog deshabilitado
-    CONFIG  LVP = OFF            ; Programación en bajo voltaje deshabilitada
-    CONFIG  PBADEN = OFF         ; PORTB configurado como digital
+    CONFIG  FOSC = INTOSCIO_EC   ; Usa el oscilador interno
+    CONFIG  WDT = OFF            ; Deshabilitar el Watchdog Timer
+    CONFIG  LVP = OFF            ; Deshabilitar la programación en bajo voltaje
+    CONFIG  PBADEN = OFF         ; Configurar PORTB como digital
 
 ;=========================================================
 ; Vector de Reinicio
@@ -26,7 +26,7 @@
 
     PSECT  resetVec, class=CODE, reloc=2
     ORG     0x00
-    GOTO    Inicio
+    GOTO    Inicio               ; Saltar a la rutina principal
 
 ;=========================================================
 ; Código Principal
@@ -42,8 +42,8 @@ Inicio:
     MOVLW   0x72        ; IRCF = 111 (8 MHz) | SCS = 10 (Oscilador interno)
     MOVWF   OSCCON
 
-    CLRF    TRISB       ; PORTB como salida
-    CLRF    LATB        ; LED apagado inicialmente
+    CLRF    TRISB       ; Configurar PORTB como salida
+    CLRF    LATB        ; Apagar LED inicialmente
 
 Secuencia:
 
@@ -51,87 +51,90 @@ Secuencia:
 ; 5 Parpadeos de 1 segundo
 ;---------------------------------------------
 
-    MOVLW   5
-    MOVWF   Contador1
+    MOVLW   5                   ; Cargar contador con 5 repeticiones
+    MOVWF   ContadorParp1       ; Guardar en variable de control
 
 Parpadeo1s:
 
-    BSF     LATB,0
-    CALL    Retardo_1s
+    BSF     LATB,0              ; Encender LED
+    CALL    Retardo_1s          ; Esperar 1 segundo
 
-    BCF     LATB,0
-    CALL    Retardo_1s
+    BCF     LATB,0              ; Apagar LED
+    CALL    Retardo_1s          ; Esperar 1 segundo
 
-    DECFSZ  Contador1,F
-    GOTO    Parpadeo1s
-
+    DECFSZ  ContadorParp1,F     ; Decrementar contador
+    GOTO    Parpadeo1s          ; Repetir hasta completar 5
 
 ;---------------------------------------------
 ; 2 Parpadeos de 2 segundos
 ;---------------------------------------------
 
-    MOVLW   2
-    MOVWF   Contador2
+    MOVLW   2                   ; Cargar contador con 2 repeticiones
+    MOVWF   ContadorParp2       ; Guardar en variable de control
 
 Parpadeo2s:
 
-    BSF     LATB,0
-    CALL    Retardo_2s
+    BSF     LATB,0              ; Encender LED
+    CALL    Retardo_2s          ; Esperar 2 segundos
 
-    BCF     LATB,0
-    CALL    Retardo_2s
+    BCF     LATB,0              ; Apagar LED
+    CALL    Retardo_2s          ; Esperar 2 segundos
 
-    DECFSZ  Contador2,F
-    GOTO    Parpadeo2s
+    DECFSZ  ContadorParp2,F     ; Decrementar contador
+    GOTO    Parpadeo2s          ; Repetir hasta completar 2
 
-    GOTO    Secuencia
+    GOTO    Secuencia           ; Reiniciar toda la secuencia
 
 ;=========================================================
-; Subrutina Retardo 1 Segundo (Aprox.)
+; Subrutina Retardo de 1 Segundo (Aprox.)
 ;=========================================================
 
 Retardo_1s:
 
-    MOVLW   8
-    MOVWF   Contador1     
+    MOVLW   8                   ; Nivel externo del retardo
+    MOVWF   Delay1
 
 Loop1:
-    MOVLW   250
-    MOVWF   Contador2     
+    MOVLW   250                 ; Nivel intermedio
+    MOVWF   Delay2
 
 Loop2:
-    MOVLW   250
-    MOVWF   Contador3
+    MOVLW   250                 ; Nivel interno
+    MOVWF   Delay3
 
 Loop3:
-    DECFSZ  Contador3,F
+    DECFSZ  Delay3,F            ; Decrementar contador interno
     GOTO    Loop3
 
-    DECFSZ  Contador2,F
+    DECFSZ  Delay2,F            ; Decrementar contador intermedio
     GOTO    Loop2
 
-    DECFSZ  Contador1,F
+    DECFSZ  Delay1,F            ; Decrementar contador externo
     GOTO    Loop1
 
-    RETURN
+    RETURN                      ; Retornar al programa principal
 
 ;=========================================================
-; Subrutina Retardo 2 Segundos
+; Subrutina Retardo de 2 Segundos
+; (Llama dos veces al retardo de 1 segundo)
 ;=========================================================
 
 Retardo_2s:
-    CALL    Retardo_1s
-    CALL    Retardo_1s
+    CALL    Retardo_1s          ; Primer segundo
+    CALL    Retardo_1s          ; Segundo segundo
     RETURN
 
 ;=========================================================
-; Variables en RAM
+; Definición de Variables
 ;=========================================================
 
     PSECT udata
 
-Contador1:   DS 1
-Contador2:   DS 1
-Contador3:   DS 1
+ContadorParp1:   DS 1   ; Control de los 5 parpadeos
+ContadorParp2:   DS 1   ; Control de los 2 parpadeos
 
-    END
+Delay1:          DS 1   ; Contador externo del retardo
+Delay2:          DS 1   ; Contador intermedio del retardo
+Delay3:          DS 1   ; Contador interno del retardo
+
+    END                     ; Fin del programa
